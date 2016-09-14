06592c7c903b1f2946b1b249f899959e769395e2
Marat Khusnetdinov <marat@khusnetdinov.ru>
2016-09-14 13:58:22 +0300

    installation script

--- install.sh
+++ install.sh
@@ -4,3 +4,13 @@ function die() {
   echo "${@}"
   exit 1
 }
+
+git clone --recursive https:https://github.com/khusnetdinov/.tmux \
+  || die "Could not clone repository to ${HOME}/.tmux"
+
+brew install tmux || die "Could not install tmux to OS"
+
+ln -s "${HOME}/.tmux/tmux.conf" "${HOME}/.tmux.conf" \
+  || die "Cpuld not link .tmux.conf file"
+echo ".tmux.conf has been linked to .tmux/tmux.conf"
+
